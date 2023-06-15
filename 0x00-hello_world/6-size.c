0465c836f547f56bcaff3ec1ce2275704d78d12f
waruguru-lewis-gatimu <warugurulewis@gmail.com>
2023-06-15 04:57:50 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints the various sizes of computer types.
+ * main - A program that prints the various sizes of computer types
  * Return: 0 (success)
  */
 int main(void)

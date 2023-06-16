1d5345dd9c5e6679358ffae4094b55bd0b9390b2
waruguru-lewis-gatimu <warugurulewis@gmail.com>
2023-06-16 02:15:05 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -7,23 +6,24 @@
  */
 int main(void)
 {
-int n, m;
-for (n = 48; n <= 56; n++)
-{
-for (m = 49; m <= 57; m++)
-{
-if (m > n)
-{
-putchar(n);
-putchar(m);
-if (n != 56 || m != 57)
-{
-putchar(',')
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
